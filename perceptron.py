ee00f172d98fbd4eb0676c77cfbb9efdbd73a2c6
Ilya Pekelny <pekelny@gmail.com>
2016-07-22 23:42:29 +0300

    Neuron. Base structure.

---
+++ perceptron.py
@@ -0,0 +1,9 @@
+class Neuron(object):
+    def __init__(self, size):
+        raise NotImplementedError
+
+    def learn(self, input_signal):
+        raise NotImplementedError
+
+    def percive(self, input_signal):
+        raise NotImplementedError
