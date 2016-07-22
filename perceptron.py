87a0a34c69cee1b7bbdc382599a3f2ae99b42687
Ilya Pekelny <pekelny@gmail.com>
2016-07-22 23:59:58 +0300

    Functionality description

--- perceptron.py
+++ perceptron.py
@@ -1,9 +1,28 @@
 class Neuron(object):
     def __init__(self, size):
+        # default weights
+        # default threshold
+        # create signal matrix
         raise NotImplementedError
 
     def learn(self, input_signal):
         raise NotImplementedError
 
     def percive(self, input_signal):
+        # get input signal in initial format (image)
+        # split it to a matrix of pixels
+        # associate it to the weights matrix
+        raise NotImplementedError
+
+    def _mul_signal_weight(self, input_signal):
+        # Multiply pixel signals to the weight of an appropriate elements of
+        # weight matrix
+        raise NotImplementedError
+
+    def _sum_signal_weight(self, input_signal):
+        raise NotImplementedError
+
+    def get_result(self):
+        # Compare result with the threshold
+        # Return boolean
         raise NotImplementedError
