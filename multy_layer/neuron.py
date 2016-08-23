70c1980fb3ec8c5603ea2138c914aad8dc6ce97a
Ilya Pekelny <pekelny@gmail.com>
2016-08-23 18:06:38 +0300

    Perceive image

--- multy_layer/neuron.py
+++ multy_layer/neuron.py
@@ -1,5 +1,6 @@
 import abc
 import math
+from pillow import Image
 import unittest
 
 import numpy as np
@@ -60,14 +61,14 @@ class InputNeuron(object):
 
     def _standardize_size(self, image):
         image.thumbnail(self.image_size, Image.ANTIALIAS)
-        bordered = Image.new('RGBA', cls.size, (255, 255, 255, 0))
+        bordered = Image.new('RGBA', self.image_size, (255, 255, 255, 0))
         borders_size = (
             (self.image_size[0] - image.size[0]) / 2,
             (self.image_size[1] - image.size[1]) / 2)
         bordered.paste(image, borders_size)
         return bordered
 
-    def _get_rgba(self, input_):
+    def _get_rgba(self, file_path):
         image = Image.open(file_path)
         standardized_image = self._standardize_size(image)
         return np.array(standardized_image.getdata())
@@ -76,15 +77,15 @@ class InputNeuron(object):
     def _get_background(input_signal):
         view_shape = [('', input_signal.dtype)]*input_signal.shape[1]
         view = input_signal.view(view_shape)
-        unique_a = numpy.unique(view, return_counts=True)
+        unique_a = np.unique(view, return_counts=True)
         max_count = unique_a[0][unique_a[1].argmax(axis=0)].tolist()
-        return numpy.array(max_count, dtype=float)
+        return np.array(max_count, dtype=float)
 
     def perceive(self, file_path):
         rgba = self._get_rgba(file_path)
         background = self._get_background(rgba)
-        diff = numpy.subtract(rgba, background)
-        abs_diff = numpy.absolute(diff) / 256.0
+        diff = np.subtract(rgba, background)
+        abs_diff = np.absolute(diff) / 256.0
         return abs_diff
 
 
@@ -97,6 +98,10 @@ class HiddenNeuron(Neuron):
 
 
 class OutputNeuron(Neuron):
+    def __init__(self, init_data, *args):
+        # Init output
+        super(OutputNeuron, self).__init__(*args)
+
     def perceive(self, input_):
         raise NotImplementedError
 
@@ -105,26 +110,26 @@ class OutputNeuron(Neuron):
 
 
 class Network(object):
-    def __init__(self, layer_size):
+    def __init__(self, layer_size, output_init_data):
         self.layer_size = layer_size
-        self.input_neurons = map(self.create_input, xrange(self.layer_size))
-        self.hidden_neurons = map(self.create_hidden, xrange(self.layer_size))
-        self.outpur_neurons = map(self.create_output, xrange(self.layer_size))
+        self.io_size = len(output_init_data)
+        self.input_layer = map(self.create_input, xrange(self.io_size))
+        self.hidden_layer = map(self.create_hidden, xrange(self.layer_size))
+        self.output_layer = map(self.create_output, output_init_data)
 
     def create_input(self, idx):
-        return InputNeuron(self.layer_size, self.layer_size)
+        return InputNeuron(self.io_size, self.layer_size)
 
     def create_hidden(self, idx):
-        return HiddenNeuron(self.layer_size, self.layer_size)
+        return HiddenNeuron(self.io_size, self.layer_size)
 
-    def create_output(self, idx):
-        return OutputNeuron(self.layer_size, self.layer_size)
+    def create_output(self, init_data):
+        return OutputNeuron(init_data, self.io_size, self.layer_size)
 
     def learn(self):
         raise NotImplementedError
 
-    def recognise(self):
-        raise NotImplementedError
-
-
-class Tests()
+    def recognise(self, file_path):
+        input_ = (neuron.perceive for neuron in self.input_layer)
+        hidden = (neuron.perceive(input_) for neuron in self.hidden_layer)
+        return (neuron.perceive(hidden) for neuron in self.output_layer)
